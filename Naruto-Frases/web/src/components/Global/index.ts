fde4b3bd642b470fc747a85ba206d3172335e82b
BhryanS2 <bhryanstepenhen@gmail.com>
2021-10-15 10:52:56 -0300

    começando o projeto de frases do naruto

---
+++ Naruto-Frases/web/src/components/Global/index.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: #332c36;
+    padding: 0;
+    margin: 0;
+    font-family: "New Tegomin", serif;
+    min-height: 100vh;
+    border: 1px solid #332c36;
+  }
+`;
